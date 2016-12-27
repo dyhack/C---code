c174526dbfc0e9bd0ca97a0ec932458022c00218
傲娇的programer <534737859@qq.com>
2016-12-27 19:41:32 +0800

    日常更新

---
+++ test/strarry/strarry/Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Collections;
+
+namespace strarry
+{
+    class Program
+    {
+        delegate  void  D(int  i);
+              public static void Main()
+                {
+                 // D d1 = new D(R);
+                 // d1(12);
+                  int[] num = new int[] { 1, 3, 5 };
+                  ArrayList arr = new ArrayList();
+                  for (int i = 0; i < num.Length; i++)
+                  { arr.Add(num[i]); }
+                  arr.Insert(1, 4);
+                  foreach(int str in arr)
+                  {
+                      Console.WriteLine(str);
+                  }
+
+
+                }
+                  public static void R(int  t)
+                   {	    V(21);		}
+                  public static void V(int  i)
+                  {Console.WriteLine(i.ToString());	
+						Console.ReadLine();
+    }
+      			
+
+    }
+}
---
+++ test/test/test/Program.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Version3
+{
+    abstract class A
+    {
+        public abstract void Method();
+     }
+    class B : A
+    {
+        public override void Method()
+        {
+            throw new NotImplementedException();
+        }
+    }
+    class C : B
+    {
+        public override void Method()
+        {
+            Console.WriteLine("C.Method");
+        }
+    }
+    class VersionControl
+    {
+        public static void Main()
+        {
+           
+            B b = new C();
+            A c = b;
+            b.Method();
+            c.Method();
+            Console.Read();
+             
+        }
+    }
+}
+
+   
+
+
+
